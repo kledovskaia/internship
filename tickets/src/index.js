cfdd6fe27304f940be319194b220ba3b05aec899
qext <MaddisonMatthewsWebDev@gmail.com>
2022-01-06 10:45:33 +0300

    theme context

--- tickets/src/index.js
+++ tickets/src/index.js
@@ -4,15 +4,18 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { FirebaseRedux } from './connect/FirebaseRedux';
+import { ThemeContextProvider } from './context/Theme';
 import { store } from './redux/store';
 
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
       <FirebaseRedux>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>  
+        <ThemeContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>  
+        </ThemeContextProvider>
       </FirebaseRedux>
     </Provider>
   </StrictMode>,
