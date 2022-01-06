e42b81d5743e5c447c094275851ed13ded2907b1
qext <MaddisonMatthewsWebDev@gmail.com>
2022-01-06 09:37:28 +0300

    transform for tickets data format

--- tickets/src/hooks/useFirebase.js
+++ tickets/src/hooks/useFirebase.js
@@ -4,11 +4,17 @@ import { useDocumentData, useCollectionData } from 'react-firebase-hooks/firesto
 import { useContext, useEffect, useState } from "react";
 import { TicketIdContext } from "../context/TicketId";
 
+const transform = ({ updatedAt, createdAt, ...doc }) => ({
+  ...doc,
+  createdAt: (createdAt.seconds * 1000 + createdAt.nanoseconds / 1000),
+  ...(updatedAt ? { updatedAt: (updatedAt.seconds * 1000 + updatedAt.nanoseconds / 1000) } : {})
+})
+
 export const useFirebase = () => {
   const { ticketId } = useContext(TicketIdContext);
   const [user, authLoading, authError] = useAuthState(auth);
-  const [ticket, ticketLoading, ticketError] = useDocumentData(getTicketQuery(ticketId));
-  const [ticketCollection, ticketCollectionLoading, ticketCollectionError] = useCollectionData(getTicketCollectionQuery());
+  const [ticket, ticketLoading, ticketError] = useDocumentData(getTicketQuery(ticketId), { transform });
+  const [ticketCollection, ticketCollectionLoading, ticketCollectionError] = useCollectionData(getTicketCollectionQuery(), { transform });
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState([]);
 
