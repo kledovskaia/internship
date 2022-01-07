8d931dae7a9f09d82614cf0fb4567ccdb7ce2c49
qext <MaddisonMatthewsWebDev@gmail.com>
2022-01-07 07:56:06 +0300

    basic routes

--- tickets/src/App.js
+++ tickets/src/App.js
@@ -1,10 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import { New } from "./pages/New";
+import { Ticket } from "./pages/Ticket";
+import { Tickets } from "./pages/Tickets";
 
 export const App = () => {
   return (
     <Routes>
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/tickets" element={<Tickets />}/>
+      <Route path="/tickets/:id" element={<Ticket />}/>
+      <Route path="/new" element={<New />}/>
       <Route path="/" element={<h1>Hello world!</h1>}/>
-      <Route path="*" element={<Navigate to="/"/>}/>
+      <Route path="*" element={<Navigate to="/dashboard"/>}/>
     </Routes>
   );
 }
