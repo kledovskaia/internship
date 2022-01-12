21a1580f16827fc36491725f10040e43fdea7008
qext <MaddisonMatthewsWebDev@gmail.com>
2022-01-12 23:47:52 +0300

    Search component

--- tickets/src/components/Search/Search.tsx
+++ tickets/src/components/Search/Search.tsx
@@ -1,7 +1,10 @@
-import { Input } from '@mui/material';
+import {
+  FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput,
+} from '@mui/material';
 import {
   useEffect, useState,
 } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
 
 type Props = {
   handleChange: (name: string, value: unknown) => void
@@ -14,5 +17,23 @@ export function Search({ handleChange }: Props) {
     handleChange('search', search);
   }, [search]);
 
-  return <Input value={search} onChange={(e) => setSearch(e.target.value)} />;
+  return (
+    <FormControl size="small" variant="outlined">
+      <InputLabel htmlFor="search">Search</InputLabel>
+      <OutlinedInput
+        id="search"
+        type="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        startAdornment={(
+          <InputAdornment position="start">
+            <IconButton edge="start">
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+    )}
+        label="Password"
+      />
+    </FormControl>
+  );
 }
